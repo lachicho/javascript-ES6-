81515acadc20497c378146640839e96c62cfdd95
lachicho <mcmoneta@gmail.com>
2021-02-11 11:13:28 +0100

    exercici 1

---
+++ js/script.js
@@ -0,0 +1,50 @@
+//Exercici 1
+//Converteix a funcions fletxa:
+
+// function multiply(num1, num2) {
+//    return num1 * num2;
+// }
+const multiply = (num1, num2) => `El resultado es: ${num1 * num2}`;
+console.log(multiply(7, 4));
+
+
+// function toCelsius(fahrenheit) {
+//    return (5/9) * (fahrenheit-32);
+// }
+const toCelsius = (fahrenheit) => `La temperatura en Celsius es ${(fahrenheit - 32) * 5 / 9} grados`;
+console.log(toCelsius(59));
+
+// //This function returns a string padded with leading zeros
+// function padZeros(num, totalLen){
+//    var numStr = num.toString();
+//    var numZeros = totalLen - numStr.length;
+//    for (var i = 1; i <= numZeros; i++) {
+//       numStr="0" + numStr; 
+//     } 
+//    return numStr;
+// } 
+
+const padZeros = (num, totalLen) => `La respuesta es ${"0".repeat(totalLen - num.toString().length) + num.toString()}`;
+console.log(padZeros(25, 5));
+
+let num = 14;
+let numStr = num.toString();
+
+console.log(numStr.padStart(6, 0));
+
+
+// function power(base, exponent) { 
+//    var result=1; 
+//    for (var i=0; i < exponent; i++) { 
+//          result *=base; 
+//    } 
+//    return result;
+// } 
+let result = (b, e) => b ** e;
+console.log(result(4, 3));
+
+// function greet(who) {
+//     console.log("Hello " + who);
+// }
+const greet = who => `Hello ${who}`;
+console.log(greet("Maria"));
