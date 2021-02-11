33dbe7af4d43cfcbc653f578cab52f6c4cf8b5e7
lachicho <mcmoneta@gmail.com>
2021-02-11 11:21:35 +0100

    exercici 6

--- js/script.js
+++ js/script.js
@@ -101,8 +101,44 @@ async function a() {
     })
     let result = await b;
     console.log(result);
- }
+}
  
- a();
+a();
+/*------------*/
+// Exercici 6
+// Tenim un array de tasques i volem aconseguir un array que contingui únicament els noms de les tasques. 
 
+// - Usar .forEach() per obtenir aquest array.
+
+// - Usar .map() per obtenir aquest array.
+
+
+var tasks = [
+    {
+       'name' : 'Start React web',
+       'duration' : 120
+    },
+    {
+       'name' : 'Work out',
+       'duration' : 60
+    },
+    {
+       'name' : 'Procrastinate on facebook',
+       'duration' : 240
+    }
+    ];
+    
+    
+    tasks.forEach(function(task) {
+        console.log(task.name);
+    });
+    
+    
+     
+    const taskName = tasks.map(function(task) {
+        return task.name;  
+    });
+    console.log(taskName);
+     
+    
 
